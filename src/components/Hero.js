5b048437d31e49ca5ba7996fb63cc9b3f7ca9880
Arfaz Khan <arfuzkhan@gmail.com>
2024-05-26 14:50:52 +0530

    updated landing page content

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import './styles/Hero.css';
 
-
 const MovieListPage = () => {
   return (
     <div className="hero">
       <h1 className="hero-title">Lights, Camera, Watchlist!</h1>
-      <p className="hero-paragraph">Welcome to 'The Watchlist' - where every click is a ticket to cinematic bliss! Dive into a world of movie magic, where the popcorn is always fresh and the plots are never predictable. Browse through our curated collection of blockbusters, indie gems, and everything in between. It's time to grab your favorite snacks, cozy up, and embark on a movie marathon like no other! Let 'The Watchlist' be your guide to endless entertainment and unforgettable cinematic experiences.</p>
+      <p className="hero-paragraph">Welcome to 'The Watchlist.' - your ticket to cinematic bliss! Explore our curated collection, add movies to your watchlist, and embark on a personalized cinematic journey. Let 'The Watchlist' be your guide to endless entertainment!</p>
     </div>
   );
 };
